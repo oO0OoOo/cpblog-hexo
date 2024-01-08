067ab42c47003023ab43325545f2c4610aca903b
yehuayu <epya15@gmail.com>
2024-01-08 20:27:58 +0800

    test new post way

--- shell/hexo-deploy.sh
+++
@@ -1,8 +0,0 @@
-cd /www/cypunberk_blog
-hexo clean
-hexo g
-hexo d
-echo 删除/www/wwwroot/cypunberk.cn目录下所有内容
-rm -rf /www/wwwroot/cypunberk.cn/*
-echo 拷贝最新发布内容到/www/wwwroot/cypunberk.cn目录下
-mv /www/cypunberk_blog/public/* /www/wwwroot/cypunberk.cn
