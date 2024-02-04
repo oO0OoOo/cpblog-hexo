81c32a7dfafaaa131f87efd34994d73766dfd716
yehuayu <epya15@gmail.com>
2024-02-04 17:45:52 +0800

    conventional update by deploy.sh

--- public/bubbly-bg/index.js
+++ public/bubbly-bg/index.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () { // 这个表示在html加载完成后执行
     bubbly({
-        colorStart: '#4c004c',
-        colorStop: '#1a001a',
-        
+        colorStart: '#fff4e6',
+        colorStop: '#ffe9e4',
+        blur:1,
+        compose: 'source-over',
         bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
       });
 })
