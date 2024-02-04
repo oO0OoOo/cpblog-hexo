bdbfc0b74ea1ec71f4eb256cf4f3109ab4d8c640
yehuayu <epya15@gmail.com>
2024-02-04 17:36:59 +0800

    conventional update by deploy.sh

--- themes/bamboo/source/bubbly-bg/index.js
+++ themes/bamboo/source/bubbly-bg/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () { // 这个表示在html加载完成后执行
     bubbly({
         colorStart: '#CEEEFF',
-        colorStop: '#ACE2FF',
-        blur:1,
+        colorStop: '#E8F7FF',
+        blur:2,
         compose: 'source-over',
         bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
       });
