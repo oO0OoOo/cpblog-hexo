9787b1bc6885ddaf79a31dfb7103754f37a3a8f0
yehuayu <epya15@gmail.com>
2024-02-04 17:59:08 +0800

    conventional update by deploy.sh

--- public/bubbly-bg/index.js
+++ public/bubbly-bg/index.js
@@ -6,6 +6,6 @@ document.addEventListener("DOMContentLoaded", function () { // 这个表示在ht
 		gradientStop: "#422",
         blur:1,
         compose: 'source-over',
-        bubbleFunc:() => `hsla(${Math.random() * 230}, 100%, 50%, .1)`
+        bubbleFunc:() => `hsla(${Math.random() * 230}, 10%, 10%, .1)`
       });
 })
